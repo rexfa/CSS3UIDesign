fd962e050384155b0f6221d9a23da1199865e395
rexfa <rex_fa@hotmail.com>
2019-12-15 00:16:04 +0800

    开始转为Electron项目

--- script/ui.js
+++ script/ui.js
@@ -2,10 +2,14 @@
 function operationonclick(taskname) {
     var controlpanelID = "controlpanel" + taskname;
     var controlpanel = document.getElementById(controlpanelID);
+    var taskpanelID = "taskpanel" + taskname
+    var taskpanel = document.getElementById(taskpanelID);
     if (controlpanel.className == "controlpanel-hidden") {
+        taskpanel.style.height = 350;
         controlpanel.className = "controlpanel-display";
     } else {
         controlpanel.className = "controlpanel-hidden";
+        taskpanel.style.height = 200;
     }
 
 }
