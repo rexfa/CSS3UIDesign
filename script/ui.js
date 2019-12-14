b795e0aad8518a1e5fcf25b6738934a771f970da
rexfa <rex_fa@hotmail.com>
2019-12-14 22:41:44 +0800

    确定风格

--- script/ui.js
+++ script/ui.js
@@ -1,1 +1,11 @@
 /* 添加动画用 */
+function operationonclick(taskname) {
+    var controlpanelID = "controlpanel" + taskname;
+    var controlpanel = document.getElementById(controlpanelID);
+    if (controlpanel.className == "controlpanel-hidden") {
+        controlpanel.className = "controlpanel-display";
+    } else {
+        controlpanel.className = "controlpanel-hidden";
+    }
+
+}
