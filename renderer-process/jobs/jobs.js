f1be85eed9c96ea0261eee585b08c275dca0da05
rexfa <rex_fa@hotmail.com>
2019-12-20 00:38:04 +0800

    编写任务栏 基础

--- renderer-process/jobs/jobs.js
+++ renderer-process/jobs/jobs.js
@@ -7,18 +7,3 @@
 
 //let jobinfoclone = document.importNode(jobinfotemplate.content, true)
 //document.querySelector('.jobspanel').appendChild(jobinfoclone)
-
-/* 添加动画用 */
-function operationonclick(taskname) {
-    var controlpanelID = "controlpanel" + taskname;
-    var controlpanel = document.getElementById(controlpanelID);
-    var taskpanelID = "taskpanel" + taskname
-    var taskpanel = document.getElementById(taskpanelID);
-    if (controlpanel.className == "controlpanel-hidden") {
-        taskpanel.style.height = 350;
-        controlpanel.className = "controlpanel-display";
-    } else {
-        controlpanel.className = "controlpanel-hidden";
-        taskpanel.style.height = 200;
-    }
-}
