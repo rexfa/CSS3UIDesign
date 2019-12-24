4afe5cd2ec5c4bbc7b38f76495a009d666e9c640
rexfa <rex_fa@163.com>
2019-12-24 17:54:12 +0800

    解决自动检查目录问题

--- renderer-process/jobs/jobs.js
+++ renderer-process/jobs/jobs.js
@@ -7,3 +7,30 @@
 
 //let jobinfoclone = document.importNode(jobinfotemplate.content, true)
 //document.querySelector('.jobspanel').appendChild(jobinfoclone)
+var CheckDirIntervalID = 0;
+
+function callJobsDir() {
+    //ipcRenderer.send('asy-message-loadjobs', 'ping');
+    //document.getElementById("jobs-log").innerHTML="Call setInterval";
+    let files = readJobDataDir();
+    for (var key in files) {
+        console.log(files[key]);
+        let j = getJobInfoPanelByJobName(files[key]);
+        if (!j) {
+            addJobInfoPanel(files[key]);
+        }
+    }
+    this.CheckDirIntervalID = (callJobsDir, 5000);
+}
+
+function readJobDataDir() {
+    //var path = require('path')
+    window.clearInterval(this.CheckDirIntervalID);
+    var fs = require('fs');
+    var dataPath = ".\\job_data";
+    //document.getElementById("jobs-log").innerHTML=dataPath;
+    var files = fs.readdirSync(dataPath);
+    return files;
+}
+//每5秒检查一下目录
+this.CheckDirIntervalID = setInterval(callJobsDir, 5000);
