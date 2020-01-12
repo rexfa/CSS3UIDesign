ba19fcfecd78c184ead8b7a7b69fa44e89aab1cd
rexfa <rex_fa@hotmail.com>
2020-01-12 16:12:37 +0800

    稍微完善了一下chart

--- renderer-process/system/app-sys-information.js
+++ renderer-process/system/app-sys-information.js
@@ -1,4 +1,3 @@
-const os = require('os');
 var cpuinfoconfig = {
     type: 'line',
     data: {},
@@ -42,10 +41,65 @@ var cpuinfoconfig = {
         }
     }
 };
+var meminfoconfig = {
+    type: 'line',
+    data: {},
+    options: {
+        responsive: true,
+        legend: {
+            labels: {
+                fontColor: '#ff8c00'
+            }
+        },
+        title: {
+            display: true,
+            text: '内存状态',
+            fontColor: '#ff8c00'
+        },
+        tooltips: {
+            mode: 'index',
+            intersect: false
+        },
+        hover: {
+            mode: 'nearest',
+            intersect: true
+        },
+        scales: {
+            xAxes: [{
+                display: true,
+                scaleLabel: {
+                    display: true,
+                    labelString: '时间',
+                    fontColor: '#ff8c00'
+                }
+            }],
+            yAxes: [{
+                display: true,
+                scaleLabel: {
+                    display: true,
+                    labelString: '使用率',
+                    fontColor: '#ff8c00'
+                }
+            }]
+        }
+    }
+};
 
 function addDataset_appsysCPUInfo(cpus) {
+    var COLORS = [
+        '#4dc9f6',
+        '#f67019',
+        '#f53794',
+        '#537bc4',
+        '#acc236',
+        '#166a8f',
+        '#00a950',
+        '#58595b',
+        '#8549ba'
+    ];
     cpus.forEach((cpu, idx, arr) => {
-        var newColor = window.chartColors[idx];
+        var newColor = COLORS[idx];
+        //console.log(newColor);
         var newDataset = {
             label: `cpu${idx}`,
             backgroundColor: newColor,
@@ -72,38 +126,80 @@ function addData_appsysCPUInfo(cpus) {
     if (cpuinfoconfig.data.datasets.length > 0) {
         var xdata = ' ';
         cpuinfoconfig.data.labels.push(xdata);
-
+        //console.log(cpus[0].times.idle);
         cpuinfoconfig.data.datasets.forEach((dataset, idx, arr) => {
             var times = cpus[idx].times;
             var cpudata = `${((1-times.idle/(times.idle+times.user+times.nice+times.sys+times.irq))*100).toFixed(2)}`;
-            console.log(cpudata);
+
             dataset.data.push(cpudata);
         });
 
         //window.cpuinfoLine.update();
     }
 }
-var DataCapacity = 0;
+var CpuDataCapacity = 0;
 
 function updatacpuinfoLine() {
+    var os = require('os');
     var cpus = os.cpus();
     //console.log(cpus[0].model);
-    if (DataCapacity < 1) {
+    if (CpuDataCapacity < 1) {
         addDataset_appsysCPUInfo(cpus);
     }
-    if (DataCapacity > 20) {
+    if (CpuDataCapacity > 20) {
         removeData_appsysCPUInfo();
-        DataCapacity--;
+        CpuDataCapacity--;
     }
     addData_appsysCPUInfo(cpus);
-    DataCapacity++;
+    CpuDataCapacity++;
     window.cpuinfoLine.update();
 }
+var MemDataCapacity = 0;
 
+function updatameminfoLine() {
+    var os = require('os');
+    var totalMem = os.totalmem();
+    var freeMem = os.freemem();
+    if (MemDataCapacity < 1) {
+        var newDataset = {
+            label: `内存使用比例`,
+            backgroundColor: '#4dc9f6',
+            borderColor: '#4dc9f6',
+            data: [],
+            fill: false
+        };
+        meminfoconfig.data.datasets.push(newDataset);
+        window.meminfoLine.update();
+    }
+    if (MemDataCapacity > 20) {
+        meminfoconfig.data.labels.shift();
+        meminfoconfig.data.datasets.forEach((dataset, idx, arr) => {
+            dataset.data.shift();
+        });
+        MemDataCapacity--;
+    }
+    meminfoconfig.data.labels.push(' ');
+    //console.log(freeMem);
+    //console.log(totalMem);
+    meminfoconfig.data.datasets.forEach((dataset, idx, arr) => {
+
+        var memdata = (freeMem / totalMem).toFixed(2) * 100;
+
+        dataset.data.push(memdata);
+    });
+    //meminfoconfig.data.datasets[0].push((freeMem / totalMem).toFixed(2) * 100);
+    MemDataCapacity++;
+    window.meminfoLine.update();
+
+}
 var cpuinfointerval = 0;
+var meminfointerval = 0;
 
 window.onload = function() {
-    var ctx = document.getElementById('appsysCPUInfo-chart-canvas').getContext('2d');
-    window.cpuinfoLine = new Chart(ctx, cpuinfoconfig);
-    cpuinfointerval = this.setInterval(updatacpuinfoLine, 5000)
+    var cpuctx = document.getElementById('appsysCPUInfo-chart-canvas').getContext('2d');
+    window.cpuinfoLine = new Chart(cpuctx, cpuinfoconfig);
+    cpuinfointerval = this.setInterval(updatacpuinfoLine, 5000);
+    var memctx = document.getElementById('appsysMEMInfo-chart-canvas').getContext('2d');
+    window.meminfoLine = new Chart(memctx, meminfoconfig);
+    meminfointerval = this.setInterval(updatameminfoLine, 5000);
 };
