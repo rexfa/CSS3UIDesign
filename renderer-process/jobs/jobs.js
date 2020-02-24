1f87451a559e27173a9aca284e62da8b37fc1983
rexfa <rex_fa@hotmail.com>
2020-02-24 23:26:41 +0800

    完善代码结构

--- renderer-process/jobs/jobs.js
+++ renderer-process/jobs/jobs.js
@@ -1,15 +1,8 @@
 //引用任务模板
 
-//const jobinfoLink = document.querySelector('.template-file').querySelectorAll('link[rel="import"]')
-//const jobinfoLink = document.querySelectorAll('link[rel="jobs"]')
-//let templatefile = jobinfoLink[0].import
-//let jobinfotemplate = jobinfoLink[0].import.querySelector('.task-template')
-
-//let jobinfoclone = document.importNode(jobinfotemplate.content, true)
-//document.querySelector('.jobspanel').appendChild(jobinfoclone)
 var CheckDirIntervalID = 0;
 //每5秒检查一下目录
 this.CheckDirIntervalID = setInterval(callJobsDir, 5000);
 
 var jobstestBtn = document.getElementById('jobs-test');
-jobstestBtn.addEventListener('click',jobsTest);
+jobstestBtn.addEventListener('click', jobsTest);
